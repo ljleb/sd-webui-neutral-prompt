a164df77dae4ea2655ad5a3f5eed49b15e2f4fb6
ljleb <lebel.louisjacob@gmail.com>
2023-05-17 13:59:46 -0400

    attempt 1

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,31 @@
+import torch
+
 from modules import scripts, processing, script_callbacks, sd_samplers_kdiffusion, shared
 
 
 is_enabled = False
 
 
-def combine_denoise_hijack(self, x_out, conds_list, uncond, cond_scale):
+def combine_denoise_hijack(self, x_out, conds_list, neg_cond, cond_scale):
     if not is_enabled:
-        return original_combine_denoise(self, x_out, conds_list, uncond, cond_scale)
+        return original_combine_denoise(self, x_out, conds_list, neg_cond, cond_scale)
 
-    raise NotImplementedError
+    denoised_uncond = x_out[-neg_cond.shape[0]:]
+    denoised = torch.clone(denoised_uncond)
+
+    for i, conds in enumerate(conds_list):
+        for cond_index, weight in conds:
+            perp = get_perpendicular_component(x_out[cond_index], denoised_uncond[i])
+            perp = x_out[cond_index] - perp
+            denoised[i] += perp * (weight * cond_scale)
+
+    assert not torch.isnan(denoised).any(), "this is not triggered but a1111 still blows up"
+    return denoised
+
+
+def get_perpendicular_component(pos, neg):
+    projected_neg = pos * torch.sum(neg * pos) / torch.norm(pos) ** 2
+    return neg - projected_neg
 
 
 original_combine_denoise = getattr(sd_samplers_kdiffusion.CFGDenoiser, '__prep_neg_original_combine_denoise', sd_samplers_kdiffusion.CFGDenoiser.combine_denoised)
