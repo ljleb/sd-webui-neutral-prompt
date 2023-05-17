fce01b691bf1a9a0cd41c9458093a9d357f08484
ljleb <lebel.louisjacob@gmail.com>
2023-05-17 11:41:47 -0400

    restart ui doesnt nest like crazy, dont change settings during generation

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,18 @@
-from modules import script_callbacks, sd_samplers_kdiffusion, shared
+from modules import scripts, processing, script_callbacks, sd_samplers_kdiffusion, shared
+
+
+is_enabled = False
 
 
 def combine_denoise_hijack(self, x_out, conds_list, uncond, cond_scale):
-    if not shared.opts.data.get('prep_neg_enabled', True):
+    if not is_enabled:
         return original_combine_denoise(self, x_out, conds_list, uncond, cond_scale)
 
     raise NotImplementedError
 
 
-original_combine_denoise = sd_samplers_kdiffusion.CFGDenoiser.combine_denoised
+original_combine_denoise = getattr(sd_samplers_kdiffusion.CFGDenoiser, '__prep_neg_original_combine_denoise', sd_samplers_kdiffusion.CFGDenoiser.combine_denoised)
+setattr(sd_samplers_kdiffusion.CFGDenoiser, '__prep_neg_original_combine_denoise', original_combine_denoise)
 sd_samplers_kdiffusion.CFGDenoiser.combine_denoised = combine_denoise_hijack
 
 
@@ -18,3 +22,15 @@ def on_ui_settings():
 
 
 script_callbacks.on_ui_settings(on_ui_settings)
+
+
+class PrepNegScript(scripts.Script):
+    def title(self) -> str:
+        return "Prep-Neg"
+
+    def show(self, is_img2img: bool):
+        return scripts.AlwaysVisible
+
+    def process(self, p: processing.StableDiffusionProcessing, *args):
+        global is_enabled
+        is_enabled = shared.opts.data.get('prep_neg_enabled', True)
