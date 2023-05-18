ab0562d70340f0e5127057e8d5e07ca4d4dbb575
Cauldrath <bnjmnhanes@gmail.com>
2023-05-18 10:25:32 -0400

    Calculate separate neg perpendicular

--- scripts/main.py
+++ scripts/main.py
@@ -5,8 +5,8 @@ import gradio as gr
 
 is_enabled = False
 neutral_prompt = ''
-neutral_cond_scale = 1.0
-cfg_rescale = 0
+neutral_cond_scale = -1.0
+cfg_rescale = 0.5
 
 
 def combine_denoise_hijack(self, x_out, conds_list, uncond, cond_scale):
@@ -20,18 +20,18 @@ def combine_denoise_hijack(self, x_out, conds_list, uncond, cond_scale):
 
     del conds_list[0][0]
 
-    x_pos_std = 0
-
     for i, conds in enumerate(conds_list):
+        x_neg = x_uncond[i]
+        x_neg_cfg = x_neg + get_perpendicular_component(x_neg, x_neutral) * neutral_cond_scale
+        
         for cond_index, weight in conds:
             x_pos = x_out[cond_index]
-            x_pos_std += torch.std(x_pos)
-            x_pos_delta = x_pos - x_uncond[i]
-            x_cfg = x_pos_delta + neutral_cond_scale * get_perpendicular_component(x_pos_delta, x_neutral - x_uncond[i])
-            denoised[i] += x_cfg * (weight * cond_scale)
+            x_cfg = x_pos + get_perpendicular_component(x_pos, x_neutral) * neutral_cond_scale
+            denoised[i] += (x_cfg - x_neg_cfg) * (weight * cond_scale)
+        x_cond_std = torch.std((denoised[i] - x_neutral) / cond_scale + x_neutral)
+        x_cfg_std = torch.std(denoised[i])
+        denoised[i] *= cfg_rescale * (x_cond_std / x_cfg_std - 1) + 1
 
-    x_cfg_std = torch.std(denoised)
-    denoised *= cfg_rescale * (x_pos_std / x_cfg_std - 1) + 1
 
     return denoised
 
@@ -41,9 +41,9 @@ setattr(sd_samplers_kdiffusion.CFGDenoiser, '__neutral_prompt_original_combine_d
 sd_samplers_kdiffusion.CFGDenoiser.combine_denoised = combine_denoise_hijack
 
 
-def get_perpendicular_component(pos, neg):
-    projected_neg = pos * torch.sum(neg * pos) / torch.norm(pos) ** 2
-    return neg - projected_neg
+def get_perpendicular_component(vector, neutral):
+    assert vector.shape == neutral.shape
+    return neutral * torch.sum(neutral * vector) / torch.norm(neutral) ** 2
 
 
 def get_multicond_learned_conditioning_hijack(model, prompts, steps):
@@ -84,7 +84,7 @@ class NeutralPromptScript(scripts.Script):
             ui_enabled = gr.Checkbox(label='Enable', value=False)
             ui_neutral_prompt = gr.Textbox(label='Neutral prompt ', show_label=False, lines=3, placeholder='Neutral prompt')
             ui_neutral_cond_scale = gr.Slider(label='Neutral CFG ', minimum=-3, maximum=0, value=-1)
-            ui_cfg_rescale = gr.Slider(label='CFG Rescale ', minimum=0, maximum=1, value=0)
+            ui_cfg_rescale = gr.Slider(label='CFG Rescale ', minimum=0, maximum=1, value=0.5)
 
         return [ui_enabled, ui_neutral_prompt, ui_neutral_cond_scale, ui_cfg_rescale]
 
