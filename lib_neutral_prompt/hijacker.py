8c82eaf64f165e00578fe4a945495626e18b464b
ljleb <lebel.louisjacob@gmail.com>
2023-05-20 02:06:43 -0400

    rem hijacker

--- lib_neutral_prompt/hijacker.py
+++ lib_neutral_prompt/hijacker.py
@@ -1,45 +1,26 @@
+import functools
+
 class ModuleHijacker:
     def __init__(self, module):
         self.__module = module
         self.__original_functions = dict()
-        self.__original_attributes = dict()
 
     def hijack(self, attribute):
         if attribute not in self.__original_functions:
             self.__original_functions[attribute] = getattr(self.__module, attribute)
 
         def decorator(function):
-            def wrapper(*args, **kwargs):
-                return function(*args, **kwargs, original_function=self.__original_functions[attribute])
-
-            setattr(self.__module, attribute, wrapper)
+            setattr(self.__module, attribute, functools.partial(function, original_function=self.__original_functions[attribute]))
             return function
 
         return decorator
 
-    def hijack_attribute(self, attribute):
-        def decorator(new_value):
-            if attribute not in self.__original_attributes:
-                self.__original_attributes[attribute] = getattr(self.__module, attribute), new_value
-
-            setattr(self.__module, attribute, new_value)
-
-        return decorator
-
-    def get_original_attribute(self, attribute):
-        return self.__original_attributes[attribute][0]
-
-    def get_backup_attribute(self, attribute):
-        return self.__original_attributes[attribute][1]
-
     def reset_module(self):
         for attribute, original_function in self.__original_functions.items():
             setattr(self.__module, attribute, original_function)
+
         self.__original_functions.clear()
 
-        for attribute, (original_value, _) in self.__original_attributes.items():
-            setattr(self.__module, attribute, original_value)
-        self.__original_attributes.clear()
 
     @staticmethod
     def install_or_get(module, hijacker_attribute, on_uninstall=lambda _callback: None):
