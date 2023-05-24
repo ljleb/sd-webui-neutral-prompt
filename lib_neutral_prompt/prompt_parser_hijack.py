e30eda5808f874f2507cafbe48fbb365f94e7ec4
ljleb <set>
2023-05-24 17:34:57 -0400

    remove

--- lib_neutral_prompt/prompt_parser_hijack.py
+++ lib_neutral_prompt/prompt_parser_hijack.py
@@ -11,14 +11,6 @@ prompt_parser_hijacker = hijacker.ModuleHijacker.install_or_get(
 )
 
 
-class PromptKeyword(Enum):
-    AND = 'AND'
-    AND_PERP = 'AND_PERP'
-
-
-and_perp_regex = re.compile(rf'\b({"|".join([e.value for e in PromptKeyword])})\b')
-
-
 @prompt_parser_hijacker.hijack('get_multicond_learned_conditioning')
 def get_multicond_learned_conditioning_hijack(model, prompts, steps, original_function):
     if not global_state.is_enabled:
