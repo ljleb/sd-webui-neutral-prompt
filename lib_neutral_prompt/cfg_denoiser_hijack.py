9b8a93298ded1b3f57a08c70451816b272941d79
ljleb <lebel.louisjacob@gmail.com>
2023-07-12 15:08:27 -0400

    add sega topk filtering (#26)
    * add sega topk filtering
    
    * dont need cond_delta
    
    * refact
    
    * rename keyword to AND_TOPK
    
    ---------
    
    Co-authored-by: ljleb <set>

--- lib_neutral_prompt/cfg_denoiser_hijack.py
+++ lib_neutral_prompt/cfg_denoiser_hijack.py
@@ -194,6 +194,8 @@ class AuxCondDeltaChildVisitor:
                     aux_cond_delta += child.weight * get_perpendicular_component(cond_delta, child_cond_delta)
                 elif child.conciliation == neutral_prompt_parser.ConciliationStrategy.SALIENCE_MASK:
                     salient_cond_deltas.append((child_cond_delta, child.weight))
+                elif child.conciliation == neutral_prompt_parser.ConciliationStrategy.SEMANTIC_GUIDANCE:
+                    aux_cond_delta += child.weight * filter_abs_top_k(child_cond_delta, 0.05)
 
             index += child.accept(neutral_prompt_parser.FlatSizeVisitor())
 
@@ -231,6 +233,12 @@ def salient_blend(normal: torch.Tensor, vectors: List[Tuple[torch.Tensor, float]
 
 def get_salience(vector: torch.Tensor) -> torch.Tensor:
     return torch.softmax(torch.abs(vector).flatten(), dim=0).reshape_as(vector)
+
+
+def filter_abs_top_k(vector: torch.Tensor, k_ratio: float) -> torch.Tensor:
+    k = int(torch.numel(vector) * (1 - k_ratio))
+    top_k, _ = torch.kthvalue(torch.abs(torch.flatten(vector)), k)
+    return vector * (torch.abs(vector) >= top_k).to(vector.dtype)
 
 
 sd_samplers_hijacker = hijacker.ModuleHijacker.install_or_get(
--- lib_neutral_prompt/ui.py
+++ lib_neutral_prompt/ui.py
@@ -12,6 +12,7 @@ img2img_prompt_textbox = None
 prompt_types = {
     'Perpendicular': neutral_prompt_parser.PromptKeyword.AND_PERP.value,
     'Saliency-aware': neutral_prompt_parser.PromptKeyword.AND_SALT.value,
+    'Semantic guidance top-k': neutral_prompt_parser.PromptKeyword.AND_TOPK.value,
 }
 
 
