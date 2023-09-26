a402ea89494927a96c0c9772b208b8860c0b34f4
ljleb <lebel.louisjacob@gmail.com>
2023-09-26 19:58:34 -0400

    external_code (#48)
    Co-authored-by: ljleb <set>

---
+++ lib_neutral_prompt/external_code/__init__.py
@@ -0,0 +1,12 @@
+def fix_path():
+    import sys
+    from pathlib import Path
+
+    extension_path = str(Path(__file__).parent.parent.parent)
+    if extension_path not in sys.path:
+        sys.path.append(extension_path)
+
+
+fix_path()
+del fix_path
+from .api import *
--- scripts/neutral_prompt.py
+++ scripts/neutral_prompt.py
@@ -66,6 +66,7 @@ def composable_lora_process_hijack(p: processing.StableDiffusionProcessing, *arg
 
     exprs = prompt_parser_hijack.parse_prompts(p.all_prompts)
     all_prompts, p.all_prompts = p.all_prompts, prompt_parser_hijack.transpile_exprs(exprs)
-    original_function(p, *args, **kwargs)
+    res = original_function(p, *args, **kwargs)
     # restore original prompts
     p.all_prompts = all_prompts
+    return res
