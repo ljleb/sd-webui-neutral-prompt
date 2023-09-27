38aad9d0f33a2851434c3aa43746d36fb12f8022
ljleb <set>
2023-09-26 20:08:38 -0400

    fix path not remeoved

--- lib_neutral_prompt/external_code/__init__.py
+++ lib_neutral_prompt/external_code/__init__.py
@@ -1,12 +1,23 @@
+import contextlib
+
+
+@contextlib.contextmanager
 def fix_path():
     import sys
     from pathlib import Path
 
     extension_path = str(Path(__file__).parent.parent.parent)
+    added = False
     if extension_path not in sys.path:
-        sys.path.append(extension_path)
+        sys.path.insert(0, extension_path)
+        added = True
+
+    yield
+
+    if added:
+        sys.path.remove(extension_path)
 
 
-fix_path()
-del fix_path
-from .api import *
+with fix_path():
+    del fix_path
+    from .api import *
